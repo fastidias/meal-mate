33bcfce01471cffe205c89bac8de77338955cfea
alexaw1 <alexander.a.weiss@daimler.com>
2023-04-13 21:37:31 +0200

    init commit

---
+++ src/main/kotlin/com/meal/mate/MateApplication.kt
@@ -0,0 +1,11 @@
+package com.meal.mate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MateApplication
+
+fun main(args: Array<String>) {
+	runApplication<MateApplication>(*args)
+}
