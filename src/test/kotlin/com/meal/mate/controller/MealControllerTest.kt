5871f19b04bea8fa040d352f7c496434bd79060a
alexaw1 <alexander.a.weiss@daimler.com>
2023-04-14 09:47:42 +0200

    init commit

--- src/test/kotlin/com/meal/mate/controller/MealControllerTest.kt
+++ src/test/kotlin/com/meal/mate/controller/MealControllerTest.kt
@@ -1,14 +1,6 @@
 package com.meal.mate.controller
 
-import com.meal.mate.service.MealService
-import io.mockk.impl.annotations.MockK
-import io.mockk.impl.annotations.SpyK
-import io.mockk.junit5.MockKExtension
-import io.mockk.verify
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.Mockito.times
-import org.mockito.Spy
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -19,9 +11,6 @@ import org.springframework.test.web.servlet.get
 @SpringBootTest
 @AutoConfigureMockMvc
 class MealControllerTest(@Autowired val mockMvc: MockMvc) {
-    @Spy
-    lateinit var mealService: MealService
-
     @Test
     fun givenStaticList_whenCallRestGet_thenReturnStaticJson() {
         mockMvc.get("/meals")
