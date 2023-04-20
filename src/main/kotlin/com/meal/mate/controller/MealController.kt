3087ad7d5adc7320af8e93e37abb843ebffe9418
Philipp Wilke <philipp.wilke@stud.leuphana.de>
2023-04-20 22:10:21 +0200

    add rest endpoints to get or create a meal object

--- src/main/kotlin/com/meal/mate/controller/MealController.kt
+++ src/main/kotlin/com/meal/mate/controller/MealController.kt
@@ -2,13 +2,34 @@ package com.meal.mate.controller
 
 import com.meal.mate.model.Meal
 import com.meal.mate.service.MealService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
+import java.util.*
 
 @RestController
+@RequestMapping("/meals")
 class MealController(val mealService: MealService) {
-    @GetMapping("/meals")
+    @GetMapping
     fun getMeals(): List<Meal> {
         return mealService.getMeals()
+    }
+
+    @PutMapping
+    fun createMeal(@RequestBody meal: Meal): ResponseEntity<Meal> {
+        val createdMeal = mealService.createMeal(meal)
+        val uri = URI("/meals/${createdMeal.id}")
+        return ResponseEntity.created(uri).build()
+    }
+
+    @GetMapping("/{mealId}")
+    fun getMeal(@PathVariable mealId: UUID): ResponseEntity<Meal> {
+        val meal = mealService.getMeal(mealId)
+        return if (meal != null) ResponseEntity.ok(meal) else ResponseEntity.notFound().build()
     }
 }
--- src/main/kotlin/com/meal/mate/model/Meal.kt
+++ src/main/kotlin/com/meal/mate/model/Meal.kt
@@ -1,3 +1,5 @@
 package com.meal.mate.model
 
-data class Meal(val name: String, val portionSize: Int, val ingredients: List<Ingredient>)
+import java.util.*
+
+data class Meal(val id: UUID, val name: String, val portionSize: Int, val ingredients: List<Ingredient>)
