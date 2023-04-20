f3903a2d17d2e0ec8106c84a8d9ff84dad92954f
alexaw1 <alexander.a.weiss@daimler.com>
2023-04-20 22:45:18 +0200

    adding mongodb credentials

--- src/main/kotlin/com/meal/mate/repo/MealRepository.kt
+++ src/main/kotlin/com/meal/mate/repo/MealRepository.kt
@@ -3,8 +3,9 @@ package com.meal.mate.repo
 import com.meal.mate.model.Meal
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
 
-@Component
+@Repository
 interface MealRepository: MongoRepository<Meal, String> {
     fun findByName(name: String): Meal
 }
