61526d0bc54e1e142d2ab54c307e1782fecded67
n3096 <philippwilke.1996@web.de>
2023-04-27 21:21:48 +0200

    add rest endpoints to get or create a meal object (#1)
    * add rest endpoints to get or create a meal object
    
    * add BaseValues and null value handling in MealController

--- src/main/kotlin/com/meal/mate/controller/MealController.kt
+++ src/main/kotlin/com/meal/mate/controller/MealController.kt
@@ -1,14 +1,35 @@
 package com.meal.mate.controller
 
+import com.meal.mate.BaseValues
 import com.meal.mate.model.Meal
 import com.meal.mate.service.MealService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
+import java.util.*
 
 @RestController
+@RequestMapping(BaseValues.PATH_MEALS)
 class MealController(val mealService: MealService) {
-    @GetMapping("/meals")
+    @GetMapping
     fun getMeals(): List<Meal> {
         return mealService.getMeals()
+    }
+
+    @PutMapping
+    fun createMeal(@RequestBody meal: Meal): ResponseEntity<Meal> {
+        return mealService.createMeal(meal)?.let { createdMeal ->
+            ResponseEntity.created(URI("${BaseValues.PATH_MEALS}/${createdMeal.id}")).build()
+        } ?: ResponseEntity.internalServerError().build()
+    }
+
+    @GetMapping("/{mealId}")
+    fun getMeal(@PathVariable mealId: UUID): ResponseEntity<Meal> {
+        return mealService.getMeal(mealId)?.let { meal -> ResponseEntity.ok(meal) } ?: ResponseEntity.notFound().build()
     }
 }
--- src/main/kotlin/com/meal/mate/service/MealService.kt
+++ src/main/kotlin/com/meal/mate/service/MealService.kt
@@ -3,14 +3,24 @@ package com.meal.mate.service
 import com.meal.mate.model.Ingredient
 import com.meal.mate.model.Meal
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class MealService {
     fun getMeals(): List<Meal> {
         val meal = Meal(
+            UUID.fromString("2f81508a-69e9-445f-ac82-40418c7bc42f"),
             "Knoblauchspaghetti mit frischen Tomaten",
             4,
             listOf(Ingredient("Spaghetti","500","g")))
         return listOf(meal)
+    }
+
+    fun getMeal(id: UUID): Meal? {
+        return getMeals().find { it.id == id }
+    }
+
+    fun createMeal(meal: Meal): Meal? {
+        return meal
     }
 }
