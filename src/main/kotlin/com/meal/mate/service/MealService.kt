201329f0855fd9e7c4e3b9d9128277bdbbcd37db
Tobias Gießler <15172111+tobiasgiessler@users.noreply.github.com>
2023-04-27 22:28:52 +0200

    MealService unit test

--- src/main/kotlin/com/meal/mate/service/MealService.kt
+++ src/main/kotlin/com/meal/mate/service/MealService.kt
@@ -2,7 +2,6 @@ package com.meal.mate.service
 
 import com.meal.mate.model.Ingredient
 import com.meal.mate.model.Meal
-import com.meal.mate.repo.MealItem
 import com.meal.mate.repo.MealRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -10,9 +9,7 @@ import org.springframework.stereotype.Service
 @Service
 class MealService(@Autowired val mealRepository: MealRepository) {
     fun getMeals(): List<Meal> {
-        val newMealItem = MealItem("testId1", "testName1")
-        mealRepository.save(newMealItem)
         val mealItems = mealRepository.findAll()
-        return mealItems.map { mealItem -> Meal(mealItem.name, 1, ArrayList<Ingredient>()) }
+        return mealItems.map { mealItem -> Meal(mealItem.name, 1, ArrayList()) }
     }
 }
--- src/test/kotlin/com/meal/mate/service/MealServiceTest.kt
+++ src/test/kotlin/com/meal/mate/service/MealServiceTest.kt
@@ -1,7 +1,12 @@
 package com.meal.mate.service
 
+import com.meal.mate.repo.MealItem
 import com.meal.mate.repo.MealRepository
+import com.ninjasquad.springmockk.MockkBean
 import com.ninjasquad.springmockk.SpykBean
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.SpyK
 import io.mockk.junit5.MockKExtension
 import io.mockk.verify
 import org.junit.jupiter.api.Assertions.*
@@ -9,24 +14,31 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+private const val mealName = "testName1"
+
+private const val mealId = "testId1"
 
 @SpringBootTest
-@ExtendWith(MockKExtension::class)
 class MealServiceTest {
     @Autowired
     private lateinit var mealService: MealService
-    @SpykBean
+    @MockkBean
     private lateinit var mealRepository: MealRepository
 
     @Test
     fun givenStaticList_whenCallingGetMeals_thenStaticListIsReturned() {
         // given
+        every { mealRepository.findAll() } returns listOf(MealItem(mealId, mealName))
 
         // when
         val meals = mealService.getMeals()
 
         // then
-        assertTrue(meals.isEmpty())
         verify(exactly = 1) { mealRepository.findAll() }
+        assertFalse(meals.isEmpty())
+        assertEquals(1, meals.size)
+        assertEquals(mealName, meals[0].name)
     }
 }
