e175f51571f9a2da96047860c8dee18fdb26b413
Richard <richardprax2003@gmail.com>
2023-08-12 22:17:06 +0200

    added CrossOrigin

--- src/main/kotlin/com/meal/mate/controller/MealController.kt
+++ src/main/kotlin/com/meal/mate/controller/MealController.kt
@@ -5,19 +5,15 @@ import com.meal.mate.model.Meal
 import com.meal.mate.service.MealService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.net.URI
 import java.util.*
 
 @RestController
 @RequestMapping(BaseValues.PATH_MEALS)
+
+// wird gebraucht damit man von dem lokalen Angular auf den Localhost/8080/meal zugreifen kann um sich da die Daten zu ziehen
+@CrossOrigin(origins = arrayOf("http://localhost:4200/"))
 class MealController(val mealService: MealService) {
     @GetMapping
     fun getMeals(): List<Meal> {
